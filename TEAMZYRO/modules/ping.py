83b85e25e570b2c294b8ea7530c09254d7afc933
ragini19854-prog <ragini.19854@gmail.com>
2026-05-11 14:05:07 +0530

    feat: blockquote ping message

--- TEAMZYRO/modules/ping.py
+++ TEAMZYRO/modules/ping.py
@@ -13,26 +13,31 @@ async def ping(update: Update, context: CallbackContext) -> None:
     start_time = time.time()
     media = random.choice(START_MEDIA)
     
+    ping_text = "<blockquote>❛ ᴘɪɴɢ ᴘᴏɴɢ .... 💗</blockquote>"
+
     try:
         if media.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
             message = await update.message.reply_photo(
                 photo=media,
-                caption='❛ ᴘɪɴɢ ᴘᴏɴɢ .... 💗'
+                caption=ping_text,
+                parse_mode="HTML"
             )
         else:
             message = await update.message.reply_video(
                 video=media,
-                caption='❛ ᴘɪɴɢ ᴘᴏɴɢ .... 💗'
+                caption=ping_text,
+                parse_mode="HTML"
             )
     except:
-        message = await update.message.reply_text('❛ ᴘɪɴɢ ᴘᴏɴɢ .... 💗')
+        message = await update.message.reply_text(ping_text, parse_mode="HTML")
     
     end_time = time.time()
     elapsed_time = round((end_time - start_time) * 1000, 3)
     
+    updated_text = f"<blockquote>❛ ᴘɪɴɢ ᴘᴏɴɢ .... 💗\n\n⚡ Pong! {elapsed_time}ms</blockquote>"
     try:
-        await message.edit_caption(caption=f'❛ ᴘɪɴɢ ᴘᴏɴɢ .... 💗\n\n⚡ Pong! {elapsed_time}ms')
+        await message.edit_caption(caption=updated_text, parse_mode="HTML")
     except:
-        await message.edit_text(f'❛ ᴘɪɴɢ ᴘᴏɴɢ .... 💗\n\n⚡ Pong! {elapsed_time}ms')
+        await message.edit_text(updated_text, parse_mode="HTML")
 
 application.add_handler(CommandHandler("ping", ping))
